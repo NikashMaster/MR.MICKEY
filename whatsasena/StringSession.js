f13e47b69375a40b44310e96deb9db67a551e30a
NikashMaster <87128818+NikashMaster@users.noreply.github.com>
2021-09-09 19:01:26 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return '☘️ MR.Miccy;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
